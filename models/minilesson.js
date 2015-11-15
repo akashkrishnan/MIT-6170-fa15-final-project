0b17a06370b8f8f394715e5c0f04819d4e3a42af
Miguel Medrano <medranom@mit.edu>
2015-11-15 17:41:34 -0500

    skeleton for minilesson and pageObj models

---
+++ models/minilesson.js
@@ -0,0 +1,179 @@
+'use strict';
+
+var Config = require( '../config.js' );
+var Utils = require( './utils.js' );
+var mongojs = require( 'mongojs' );
+
+var db = mongojs( Config.services.db.mongodb.uri, [ 'minilessons' ] );
+
+module.exports = {
+
+    get: get,
+    add: add,
+    remove: remove,
+
+};
+
+
+/**
+ * @callback getCallback
+ * @param {Error} err - Error object
+ * @param {object} minilesson - Minilesson object
+ */
+
+/**
+ * Gets a Minilesson object.
+ *
+ * @param {object} data - data
+ * @param {*} [data._id] - Minilesson._id
+ * @param {getCallback} done - callback
+ */
+function get( data, done ) {
+    try {
+
+        var criteria = Utils.validateObject( data, {
+            _id: { filter: 'MongoId' },
+        } );
+
+        /**
+         * Called after minilesson is found in database.
+         *
+         * @param {object} criteria -
+         */
+        var next = function ( criteria ) {
+
+            db.minilessons.findOne( criteria, function ( err, minilesson ) {
+                if ( err ) {
+                    done( err, null );
+                } else if ( minilesson ) {
+
+                    // Stringify the MongoId
+                    minilesson._id = minilesson._id.toString();
+
+                    done( null, minilesson );
+
+                } else {
+                    done( new Error( 'Minilesson not found: ' + JSON.stringify( criteria ) ), null );
+                }
+            } );
+
+        };
+        next(criteria);
+
+    } catch ( err ) {
+        done( err, null );
+    }
+}
+
+/**
+ * @callback addCallback
+ * @param {Error} err - Error object
+ * @param {object} user - newly created minilesson object
+ */
+
+/**
+ * Adds a minilesson.
+ *
+ * @param {object} data - data
+ * @param {string} data.name - Minilesson.name
+ * @param {string} data.state - Minilesson.state
+ * @param {string} data.pagesList - Minilesson.pagesList
+ * @param {addCallback} done - callback
+ */
+function add( data, done ) {
+    try {
+
+        var criteria = Utils.validateObject( data, {
+            name: {
+                type: 'string',
+                filter: function ( name ) {
+                    if ( name ) {
+                        return name.trim();
+                    }
+                },
+                required: true
+            },
+            state: {
+                type: 'string',
+                filter: function ( state ) {
+                    if ( state ) {
+                        return state.trim();
+                    }
+                },
+                required: true
+            },
+            pagesList: {
+                type: 'array',
+                required: true
+            }
+        } );
+
+        db.minilessons.insert(
+            {
+                name: criteria.name,
+                state: criteria.state,
+                pagesList: criteria.pagesList,
+                timestamps: {
+                    created: new Date(),
+                }
+            },
+            function ( err, minilesson ) {
+
+                if ( err ) {
+                    done( err, null );
+                } else {
+                    // Get the new user object the proper way
+                    get( { _id: minilesson._id }, done );
+
+                }
+
+            }
+        );
+
+    } catch ( err ) {
+        done( err, null );
+    }
+}
+
+/**
+ * @callback removeCallback
+ * @param {Error} err - Error object
+ * @param {object} minilesson - removed Minilesson object
+ */
+
+/**
+ * Removes a minilesson from the database.
+ *
+ * @param {object} data -
+ * @param {string} data._id - minilesson._id
+ * @param {removeCallback} done - callback
+ */
+function remove( data, done ) {
+    try {
+
+        var criteria = Utils.validateObject( data, {
+            _id: { type: 'string', required: true }
+        } );
+
+        // Ensure valid minilesson
+        get( criteria, function ( err, minilesson ) {
+            if ( err ) {
+                done( err, null );
+            } else {
+
+                // Remove from database
+                db.minilessons.remove( criteria, true, function ( err ) {
+                    if ( err ) {
+                        done( err, null );
+                    } else {
+                        done( null, minilesson );
+                    }
+                } );
+
+            }
+        } );
+
+    } catch ( err ) {
+        done( err, null );
+    }
+}
