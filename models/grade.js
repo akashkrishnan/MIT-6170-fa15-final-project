68594e3cfd0390db6eb7a19e4ec32a0faf1257be
Tiffany Wong <tcwong@Tiffanys-MacBook-Pro.local>
2015-11-17 18:12:39 -0500

    added grade get function

--- models/grade.js
+++ models/grade.js
@@ -3,118 +3,141 @@
 var Config = require( '../config.js' );
 var Utils = require( './utils.js' );
 var mongojs = require( 'mongojs' );
+var User = require('./user');
+var PageObj = require('./pageObj');
+var Submission = require('./submission');
+var MiniLesson = require('./minilesson');
 
 var db = mongojs( Config.services.db.mongodb.uri, [ 'grades' ] );
 
+module.exports = {
+
+    get: get,
+    compute: compute
+
+};
 
 /**
- * Adds a grade.
+ * Computes and inserts grade.
  *
  * @param {object} data - data
  * @param {string} data.studentId - Grade.studentId
- * @param {string} data.classId - Grade.classId
- * @param {string} data.period - Grade.period
- * @param {string} data.lessonId - Grade.lessonId
- * @param {string} data.lessonGrade - Grade.lessonGrade
+ * @param {string} data.minilessonId - Grade.lessonId
  * @param {addCallback} done - callback
  */
-function add( data, done ) {
-  try {
-
-    var criteria = Utils.validateObject( data, {
-      userId: {
+function compute(data, done) {
+	var criteria = Utils.validateObject( data, {
+      studentId: {
         type: 'string',
-        filter: function ( userid ) {
-          if ( userid ) {
-            return userid.trim();
-          }
-        },
         required: true
       },
-      classid: {
+      minilessonId: {
         type: 'string',
-        filter: function ( username ) {
-          if ( username ) {
-            return username.trim();
-          }
-        },
-        required: true
-      },
-      password: {
-        type: 'string',
-        filter: function ( password ) {
-          if ( password ) {
-            return password.trim();
-          }
-        },
         required: true
       }
     } );
 
-    // Make sure username and password are well-defined
-    validateCredentials( criteria.name, criteria.username, criteria.password, function ( err ) {
-      if ( err ) {
+    User.get({_id : criteria.studentId},function(error, user) {
+    	if(error) {
+    		done(error);
+    	}
+    	else {
+        try{
+      		MiniLesson.get({_id : criteria.minilessonId},function(error, minilesson) {
+  		    	if(error) {
+  		    		done(error);
+  		    	} else {
+  		    		//TODO: validate that student is in minilesson 
+  		    		submission.list(criteria, function(error, submissions) {
+  		    			if(error) {
+  		    				done(error);
+  		    			} else {
+  		    				if(submissions.length===0) {
+  		    					done(new Error("Cannot compute grade with num submissions = 0"));
+  		    				}
+  		    				else {
+  		    					var sum = 0; //# of correct for minilesson for student
+  			    				submissions.forEach(function(submission) {
+  			    					sum += submission.correct;
+  			    				})
+
+  			    				var grade = sum/submissions.length;
+
+  			    				db.grade.insert(
+                        {
+                            studentId: criteria.studentId,
+                            minilessonId: criteria.minilessonId,
+                            grade: grade
+                            // TODO: INSERT PERIOD 
+                        },
+                        function ( err, grade ) {
+
+                            if ( err ) {
+                                done( err, null );
+                            } else {
+                                // Get the new user object the proper way
+                                get( { _id: grade._id }, done );
+
+                            }
+
+                        }
+                    );
+  		    				}
+  		    			}
+  		    		});
+
+
+  		 
+  		    	}
+  		    });
+        
+    	} catch ( err ) {
         done( err, null );
-      } else {
+      }
+    })
+}
 
-        // Ensure username is unique
-        exists( { username: criteria.username }, function ( err, _exists ) {
-          if ( err ) {
-            done( err, null );
-          } else if ( _exists ) {
-            done(
-              new Error( 'User already exists: ' + JSON.stringify( { username: criteria.username } ) + '.' ),
-              null
-            );
-          } else {
-            try {
+/**
+ * @callback getCallback
+ * @param {Error} err - Error object
+ * @param {object} grade - Grade object
+ */
 
-              // Generate cryptographically strong pseudo-random salt
-              var salt = crypto.randomBytes( 256 / 8 ).toString( 'hex' );
+/**
+ * Gets a Grade object.
+ *
+ * @param {object} data - data
+ * @param {*} [data._id] - Grade._id
+ * @param {getCallback} done - callback
+ */
+function get( data, done ) {
+    try {
 
-              // Generate salted hash
-              var sha256 = crypto.createHash( 'sha256' );
-              sha256.update( salt + criteria.password );
-              var saltedPasswordHash = sha256.digest( 'hex' );
-
-              // Insert new user data into database
-              db.users.insert(
-                {
-                  name: criteria.name,
-                  username: criteria.username,
-                  salt: salt,
-                  password: saltedPasswordHash,
-                  timestamps: {
-                    created: new Date(),
-                    lastSigned: null,
-                    signed: null,
-                    active: null
-                  }
-                },
-                function ( err, user ) {
-
-                  if ( err ) {
-                    done( err, null );
-                  } else {
-
-                    // Get the new user object the proper way
-                    get( { _id: user._id }, done );
-
-                  }
-
-                }
-              );
-
-            } catch ( err ) {
-              done( new Error( 'Unable to securely add user. Please try again.' ), null );
-            }
-          }
+        var criteria = Utils.validateObject( data, {
+            _id: { filter: 'MongoId' },
         } );
 
-      }
-    } );
+        /**
+         * Called after grade is found in database.
+         *
+         * @param {object} criteria -
+         */
+        var getGrade = function ( criteria ) {
 
-  } catch ( err ) {
-    done( err, null );
-  }
+            db.minilessons.findOne( criteria, function ( err, grade ) {
+                if ( err ) {
+                    done( err, null );
+                } else if ( grade ) {
+                    done( null, grade );
+                } else {
+                    done( new Error( 'Grade not found: ' + JSON.stringify( criteria ) ), null );
+                }
+            } );
+
+        };
+        getGrade(criteria);
+
+    } catch ( err ) {
+        done( err, null );
+    }
 }
