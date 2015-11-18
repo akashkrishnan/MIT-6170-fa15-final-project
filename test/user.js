573bdf7159421d3465d3428c92da9900787e690c
Akash Krishnan <ak@aakay.net>
2015-11-17 19:13:37 -0500

    added a few more tests for user model

--- test/user.js
+++ test/user.js
@@ -42,12 +42,12 @@ describe( 'User', function () {
 
   describe( '#get()', function () {
 
-    it( 'gets a user without error', function ( done ) {
-      User.get( user, done );
+    it( 'gets a user by _id without error', function ( done ) {
+      User.get( { _id: user._id }, done );
     } );
 
     it( 'user in callback should have correct data', function ( done ) {
-      User.get( user, function ( err, _user ) {
+      User.get( { _id: user._id }, function ( err, _user ) {
         if ( err ) {
           done( err );
         } else {
@@ -55,6 +55,33 @@ describe( 'User', function () {
           assert.equal( _user.name, user.name );
           assert.equal( _user.username, user.username );
           done();
+        }
+      } );
+    } );
+
+    it( 'gets a user by username and password without error', function ( done ) {
+      User.get( { username: data.username, password: data.password }, done );
+    } );
+
+    it( 'user in callback should have correct data', function ( done ) {
+      User.get( { username: data.username, password: data.password }, function ( err, _user ) {
+        if ( err ) {
+          done( err );
+        } else {
+          assert.equal( _user._id, user._id );
+          assert.equal( _user.name, user.name );
+          assert.equal( _user.username, user.username );
+          done();
+        }
+      } );
+    } );
+
+    it( 'gets a user by username and invalid password with error', function ( done ) {
+      User.get( { username: data.username, password: 'wrong password' }, function ( err, _user ) {
+        if ( err ) {
+          done();
+        } else {
+          done( new Error( 'Should have received an error.' ) );
         }
       } );
     } );
