b353407fa7350b0e89abdc8c677327d47b8a0435
Akash Krishnan <ak@aakay.net>
2015-11-17 19:06:55 -0500

    added basic tests for user model; nam test now uses a special test database and clear it on every run

---
+++ test/user.js
@@ -0,0 +1,65 @@
+/**
+ * Created by akashkrishnan on 11/17/15.
+ */
+
+'use strict';
+
+var User = require( '../models/user.js' );
+var assert = require( 'assert' );
+
+describe( 'User', function () {
+
+  var data = {
+    name: 'Akash Krishnan',
+    username: 'akashkrishnan',
+    password: 'akashk16MIT!'
+  };
+
+  var user;
+
+  describe( '#add()', function () {
+
+    it( 'creates a new user without error', function ( done ) {
+
+      User.add( data, function ( err, _user ) {
+        if ( err ) {
+          done( err );
+        } else {
+          user = _user;
+          done();
+        }
+      } );
+
+    } );
+
+    it( 'user in callback should have correct data', function ( done ) {
+      assert.equal( user.name, data.name );
+      assert.equal( user.username, data.username );
+      done();
+    } );
+
+  } );
+
+  describe( '#get()', function () {
+
+    it( 'gets a user without error', function ( done ) {
+      User.get( user, done );
+    } );
+
+    it( 'user in callback should have correct data', function ( done ) {
+      User.get( user, function ( err, _user ) {
+        if ( err ) {
+          done( err );
+        } else {
+          assert.equal( _user._id, user._id );
+          assert.equal( _user.name, user.name );
+          assert.equal( _user.username, user.username );
+          done();
+        }
+      } );
+    } );
+
+  } );
+
+
+} );
