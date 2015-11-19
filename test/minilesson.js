8eda26f6c4fd303ca50b73c205fd1ad372e279da
Miguel Medrano <medranom@mit.edu>
2015-11-18 21:48:54 -0500

    added test cases for addPage and removePage in minilessons

--- test/minilesson.js
+++ test/minilesson.js
@@ -26,9 +26,38 @@ describe( 'Minnilesson', function() {
             it('should return callback with correct data', function(done) {
                 assert.equal( minilesson.name, minilessonData.name);
                 assert.equal( minilesson.state, minilessonData.state);
-                assert.equal( minilesson.pagesList, minilessonData.pagesList);
                 done();
             });
+        });
+    });
+
+    describe( '#get()', function(){
+        it( 'gets a minilesson by _id without error', function(done){
+            Minilesson.get({_id: minilesson._id}, done);
+        });
+        it('should return minilesson with correct data in callback', function(done) {
+            Minilesson.get({_id: minilesson._id}, function(err, _minilesson) {
+                if (err) {
+                    done( err );
+                } else {
+                    assert.equal(_minilesson._id, minilesson._id)
+                    assert.equal(_minilesson.name, minilesson.name);
+                    assert.equal(_minilesson.state, minilesson.state);
+                    done();
+                }
+            });
+        });
+    });
+
+    describe( '#addPage', function(){
+        it('adds a Page to existing minilesson without error', function(done){
+            Minilesson.addPage({_id: minilesson._id, page_id: "0"}, done);
+        });
+    })
+
+    describe('#removePage', function(){
+        it('removes a Page to existing minilesson without error', function(done){
+            Minilesson.remove({_id:minilesson._id, page_id: "0"}, done);
         })
     })
-})
+});
