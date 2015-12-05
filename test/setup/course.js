8ca11685e3c585aa80603b2d3a79fe0b184a0e5a
Tiffany Wong <tcwong@dhcp-18-189-68-238.dyn.mit.edu>
2015-12-05 14:42:14 -0500

    course test setup, copied code from course test for now

--- test/setup/course.js
+++ test/setup/course.js
@@ -5,7 +5,30 @@ module.exports = function ( done ) {
   // Make sure user is set up
   require( './user.js' )( function () {
 
-    // do some setup for course.js
+    var currentCourse = null;
+
+	  var teacherData = {
+	    name: 'Harini Suresh',
+	    username: 'hsuresh',
+	    password: 'HaRiNi999!'
+	  }
+
+	  var studentData = {
+	    name: 'Tiffany Wong',
+	    username: 'tiffanywong',
+	    password: 'Tiffany999!'
+	  }
+
+	  var studentData2 = {
+	    name: 'Alyssa Hacker',
+	    username: 'ahack',
+	    password: 'Alyssa999!'
+	  }
+
+	  var teacher;
+	  var student;
+	  var student2;
+	  var testCourse;
     done();
 
   } );
