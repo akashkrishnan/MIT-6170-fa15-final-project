1eafbace55564c1b0eb7626f1972b3560eca854f
Tiffany Wong <tcwong@dhcp-18-189-68-238.dyn.MIT.EDU>
2015-12-06 13:50:02 -0500

    wrote setup for course and minilessons tests

--- test/setup/minilesson.js
+++ test/setup/minilesson.js
@@ -1,11 +1,34 @@
 'use strict';
 
+var Course = require('../models/course.js');
+var User = require( '../models/user.js' );
+
 module.exports = function ( done ) {
 
   // Make sure course is set up
   require( './course.js' )( function () {
+  	
+  	var teacher = User.get({
+  		username: "harinisuresh",
+  		paswords: "HaRiNi999!"
+  	}, function (err, _user) {
+            if (err) {
+                throw err;
+            }
 
-    // do some setup for minilesson.js
+            Course.add({
+		  		name: "Intro to AP Physics",
+		  		teacher_id: teacher._id
+		  	}, function (err, _course) {
+		  		if (err) {
+		  			throw err;
+		  		}
+		  		course = _course;
+		  	});
+            user = _user;
+            });
+  	);
+
     done();
 
   } );
